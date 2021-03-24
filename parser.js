f9b8986ff58c2c9f2bd5841be9ac04331321dcf9
Francisco Piria <franpiria42@gmail.com>
2021-03-24 14:39:21 -0300

    it works

--- parser.js
+++ parser.js
@@ -1,21 +1,15 @@
 function parse(expr) {
-/*     if(!isBalanced(expr)) {
-        return false
-    } */
-
-    
-    
-    const isList = /^\(.*\)$/.test(expr);
+    const isList = /^\(.*\)$/.test(expr)
     if (!isList) {
         if (/[\(\)]/.test(expr)) {
-            throw 'Invalid S-exp';
+            throw 'Invalid S-exp'
         } else {
             return expr
         }
     } else {
         const result = []
         const exp = expr.substring(1, expr.length - 1)
-        const atoms = exp.split(' ')
+        const atoms = customSplit(exp)
         for (const atom of atoms) {
             result.push(parse(atom))
         }
@@ -24,7 +18,51 @@ function parse(expr) {
     
 }
 
-function isBalanced(str) {
+function customSplit(str) {
+    const result = []
+    let atom = ''
+    let canStartList = true
+    let i = 0
+    while (i < str.length) {
+        const char = str.charAt(i)
+        if (char === ' ') {
+            result.push(atom)
+            atom = ''
+            canStartList = true
+        } else if (char === '(') {
+            if (canStartList) {
+                let parCount = 1
+                for (let j = i+1; j<str.length; j++) {
+                    const auxChar = str.charAt(j)
+                    if (auxChar === '(') {
+                        parCount++
+                    } else if (auxChar === ')') {
+                        parCount--
+                    }
+                    if (parCount === 0) {
+                        atom = str.substring(i, j+1)
+                        i = j
+                        canStartList = false
+                        break
+                    }
+                }
+                if (parCount > 0) {
+                    throw 'Invalid S-exp'
+                }
+            } else {
+                throw 'Invalid S-exp'
+            }
+        } else {
+            atom += char
+            canStartList = false
+        }
+        i++
+    }
+    result.push(atom)
+    return result
+}
+
+/* function isBalanced(str) {
 
     const stack = []
     const paren_obj = { ')' : '(' }
@@ -43,20 +81,22 @@ function isBalanced(str) {
         }
     }
     return !!stack
-}
+} */
 
 
 // console.log(isBalanced('()())())'))
+
+
 try {
-    // console.log(parse("()())())"));
-    // console.log(parse("(akjsckajsc)"));
-    // console.log(parse("(lasncla lasnlas)"));
+    //console.log(parse("()())())"));
+    console.log(parse("(akjsckajsc)"));
+    console.log(parse("(lasncla lasnlas)"));
     console.log(parse("(kncas (aljcals qlnlasc (asncalscn)))"));
+    //console.log(parse("(kncas(aljcals qlnlasc (asncalscn)))"));
+    console.log(parse("(kncas (aljcals qlnlasc (asncalscn))))"));
 } catch (e) {
     console.error(e);
 }
-
-
 
 // t (a 1 b (* 2 3))
 // /^\(.*\)$/
