7168f286cd91408c8e7078369f6fb2642c39f7fc
Andres Puglia <32549160+AndresPuglia98@users.noreply.github.com>
2021-03-24 15:41:57 -0300

    Clean code

--- parser.js
+++ parser.js
@@ -1,11 +1,8 @@
 function parse(expr) {
     const isList = /^\(.*\)$/.test(expr)
+    const hasPar = /[\(\)]/.test(expr)
     if (!isList) {
-        if (/[\(\)]/.test(expr)) {
-            throw 'Invalid S-exp'
-        } else {
-            return expr
-        }
+        if (hasPar) { throw 'Invalid S-exp' } else { return expr }
     } else {
         const result = []
         const exp = expr.substring(1, expr.length - 1)
@@ -62,46 +59,16 @@ function customSplit(str) {
     return result
 }
 
-/* function isBalanced(str) {
-
-    const stack = []
-    const paren_obj = { ')' : '(' }
-
-    for (const char of str) {
-        if (char in paren_obj) {
-            
-            const top = stack.pop() || '#'
-
-            if (top != paren_obj[char]) {
-                return false
-            }
-
-        } else if(Object.values(paren_obj).includes(char)) {
-            stack.push(char)
-        }
-    }
-    return !!stack
-} */
-
-
-// console.log(isBalanced('()())())'))
-
 
 try {
+
     //console.log(parse("()())())"));
     console.log(parse("(akjsckajsc)"));
     console.log(parse("(lasncla lasnlas)"));
     console.log(parse("(kncas (aljcals qlnlasc (asncalscn)))"));
     //console.log(parse("(kncas(aljcals qlnlasc (asncalscn)))"));
     console.log(parse("(kncas (aljcals qlnlasc (asncalscn))))"));
+    
 } catch (e) {
     console.error(e);
 }
-
-// t (a 1 b (* 2 3))
-// /^\(.*\)$/
-
-// var cadena = "(a b 5 ())";
-// var result = /^\(.*\)$/.test(cadena);
-// console.log(result);
-
